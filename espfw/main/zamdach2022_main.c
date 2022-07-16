7e0b505e8b730632617aa7c8bdedeeefc398aa7c
Michael 'PoempelFox' Meier <git@mulder.franken.de>
2022-07-16 10:23:13 +0200

    Start looping and doing/sending measurements every 60 seconds. Sleep inbetween. Note: Sleep currently only tested for WIFI mode but not for POE mode yet.

--- espfw/main/zamdach2022_main.c
+++ espfw/main/zamdach2022_main.c
@@ -4,12 +4,14 @@
 #include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/event_groups.h"
 #include "esp_netif.h"
 #include "esp_eth.h"
 #include "esp_wifi.h"
 #include "esp_event.h"
 #include "esp_log.h"
 #include "esp_system.h"
+#include "esp_sleep.h"
 #include "driver/gpio.h"
 #include "driver/i2c.h"
 #include "math.h"
@@ -26,6 +28,11 @@ static const char *TAG = "zamdach2022";
 char chipid[30] = "ZAMDACH-UNSET";
 
 #define I2C_MASTER_TIMEOUT_MS 1000  /* Timeout for I2C communication */
+
+/* Bits for the network-status event-group */
+#define NETWORK_CONNECTED_BIT BIT0  /* Set when we get an IP */
+
+EventGroupHandle_t network_event_group;
 
 double reducedairpressurecalc(double press)
 {
@@ -88,6 +95,7 @@ static void wifi_event_handler(void *arg, esp_event_base_t event_base,
             break;
         case WIFI_EVENT_STA_DISCONNECTED:
             ESP_LOGI(TAG, "WiFi Disconnected: reason %u", ev_dc->reason);
+            if (ev_dc->reason == WIFI_REASON_ASSOC_LEAVE) break; /* This was an explicit call to disconnect() */
             if ((lastwifireconnect == 0)
              || ((time(NULL) - lastwifireconnect) > 5)) {
               /* Last reconnect attempt more than 5 seconds ago - try it again */
@@ -114,47 +122,14 @@ static void got_ip_event_handler(void *arg, esp_event_base_t event_base,
     ESP_LOGI(TAG, "NETMASK:" IPSTR, IP2STR(&ip_info->netmask));
     ESP_LOGI(TAG, "GW:     " IPSTR, IP2STR(&ip_info->gw));
     ESP_LOGI(TAG, "~~~~~~~~~~~");
+    xEventGroupSetBits(network_event_group, NETWORK_CONNECTED_BIT);
 }
 
-void app_main(void)
+void network_prepare(void)
 {
-    /* WiFi does not work without this because... who knows, who cares. */
-    nvs_flash_init();
-    /* Get our ChipID */
-    {
-      uint8_t mainmac[6];
-      if (esp_efuse_mac_get_custom(mainmac) != ESP_OK) {
-        /* No custom MAC in EFUSE3, use default MAC */
-        esp_read_mac(mainmac, ESP_MAC_WIFI_STA);
-      }
-      sprintf(chipid, "ZAMDACH-%02x%02x%02x%02x%02x%02x",
-                      mainmac[0], mainmac[1], mainmac[2],
-                      mainmac[3], mainmac[4], mainmac[5]);
-      ESP_LOGI(TAG, "My Chip-ID: %s", chipid);
-    }
-    // Initialize TCP/IP network interface (should be called only once in application)
-    ESP_ERROR_CHECK(esp_netif_init());
-    // Create default event loop that running in background
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-
-    i2c_config_t i2cp0conf = {
-        .mode = I2C_MODE_MASTER,
-        .sda_io_num = 13,  /* GPIO13 */
-        .scl_io_num = 16,  /* GPIO16 */
-        .sda_pullup_en = GPIO_PULLUP_ENABLE,
-        .scl_pullup_en = GPIO_PULLUP_ENABLE,
-        .master.clk_speed = 250000, /* There is really no need to hurry */
-    };
-    i2c_param_config(0, &i2cp0conf);
-    if (i2c_driver_install(0, i2cp0conf.mode, 0, 0, 0) != ESP_OK) {
-        ESP_LOGI(TAG, "Oh dear: I2C-Init for Port 0 failed.");
-    } else {
-        ESP_LOGI(TAG, "I2C master port 0 initialized");
-    }
-
+    network_event_group = xEventGroupCreate();
 #ifdef CONFIG_ZAMDACH_USEWIFI
     esp_netif_create_default_wifi_sta(); /* This seems to return a completely useless structure */
-    /* esp_netif_t * mainnetif = esp_netif_new(nicfg); */
     wifi_init_config_t wicfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&wicfg));
     // Register user defined event handers
@@ -170,8 +145,6 @@ void app_main(void)
     };
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wccfg));
-    ESP_ERROR_CHECK(esp_wifi_start());
-    ESP_ERROR_CHECK(esp_wifi_connect());
 #else /* !CONFIG_ZAMDACH_USEWIFI - we use ethernet */
     // Create new default instance of esp-netif for Ethernet
     esp_netif_config_t nicfg = ESP_NETIF_DEFAULT_ETH();
@@ -208,28 +181,114 @@ void app_main(void)
     /* start Ethernet driver state machine */
     ESP_ERROR_CHECK(esp_eth_start(eth_handle));
 #endif /* !CONFIG_ZAMDACH_USEWIFI */
+}
 
+void network_on(void)
+{
+#ifdef CONFIG_ZAMDACH_USEWIFI
+    ESP_ERROR_CHECK(esp_wifi_start());
+    ESP_ERROR_CHECK(esp_wifi_connect());
+#else /* !CONFIG_ZAMDACH_USEWIFI - we use ethernet */
+#endif /* !CONFIG_ZAMDACH_USEWIFI */
+}
+
+void network_off(void)
+{
+    xEventGroupClearBits(network_event_group, NETWORK_CONNECTED_BIT);
+#ifdef CONFIG_ZAMDACH_USEWIFI
+    ESP_ERROR_CHECK(esp_wifi_stop());
+#else /* !CONFIG_ZAMDACH_USEWIFI - we use ethernet */
+#endif /* !CONFIG_ZAMDACH_USEWIFI */
+}
+
+void app_main(void)
+{
+    time_t lastmeasts = 0;
+    /* WiFi does not work without this because... who knows, who cares. */
+    nvs_flash_init();
+    /* Get our ChipID */
+    {
+      uint8_t mainmac[6];
+      if (esp_efuse_mac_get_custom(mainmac) != ESP_OK) {
+        /* No custom MAC in EFUSE3, use default MAC */
+        esp_read_mac(mainmac, ESP_MAC_WIFI_STA);
+      }
+      sprintf(chipid, "ZAMDACH-%02x%02x%02x%02x%02x%02x",
+                      mainmac[0], mainmac[1], mainmac[2],
+                      mainmac[3], mainmac[4], mainmac[5]);
+      ESP_LOGI(TAG, "My Chip-ID: %s", chipid);
+    }
+    // Initialize TCP/IP network interface (should be called only once in application)
+    ESP_ERROR_CHECK(esp_netif_init());
+    // Create default event loop that running in background
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    i2c_config_t i2cp0conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = 13,  /* GPIO13 */
+        .scl_io_num = 16,  /* GPIO16 */
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = 250000, /* There is really no need to hurry */
+    };
+    i2c_param_config(0, &i2cp0conf);
+    if (i2c_driver_install(0, i2cp0conf.mode, 0, 0, 0) != ESP_OK) {
+        ESP_LOGI(TAG, "Oh dear: I2C-Init for Port 0 failed.");
+    } else {
+        ESP_LOGI(TAG, "I2C master port 0 initialized");
+    }
+
+    network_prepare();
 
     lps25hb_init(0);
     rg15_init();
-    rg15_requestread();
+    vTaskDelay(pdMS_TO_TICKS(3000)); // Mainly to give the RG15 time to
+    // process our initialization sequence.
 
-    /* Pressure measurement takes 1 second, so delay for slightly more than that */
-    vTaskDelay(pdMS_TO_TICKS(2111));
+    while (1) {
+      if (((time(NULL) - lastmeasts) >= 60)
+        || (lastmeasts == 0)) { /* time() starts at 0 on startup, so we need this special case. */
+        lastmeasts = time(NULL);
 
-    float press = lps25hb_readpressure();
-    float raing = rg15_readraincount();
+        /* Request update from the sensors that don't autoupdate all the time */
+        rg15_requestread();
 
-    if (press > 0) {
-      ESP_LOGI(TAG, "That converts to a measured pressure of %.3f hPa, or a calculated pressure of %.3f hPa at sea level.",
-                    press, reducedairpressurecalc(press));
-      /* submit that measurement */
-      submit_to_wpd(CONFIG_ZAMDACH_WPDSID_PRESSURE, "pressure", press);
-    }
+        network_on(); /* Turn network on, i.e. connect to WiFi */
 
-    if (raing > -0.1) {
-      ESP_LOGI(TAG, "Rain: %.3f mm", raing);
+        /* slight delay to allow the RG15 to reply */
+        vTaskDelay(pdMS_TO_TICKS(1111));
+        /* Read all the sensors */
+        float press = lps25hb_readpressure();
+        float raing = rg15_readraincount();
+
+        /* potentially wait for up to 4 more seconds if we haven't got an
+         * IP address yet */
+        xEventGroupWaitBits(network_event_group, NETWORK_CONNECTED_BIT,
+                            pdFALSE, pdFALSE,
+                            (4000 / portTICK_PERIOD_MS));
+        if (press > 0) {
+          ESP_LOGI(TAG, "That converts to a measured pressure of %.3f hPa, or a calculated pressure of %.3f hPa at sea level.",
+                        press, reducedairpressurecalc(press));
+          /* submit that measurement */
+          submit_to_wpd(CONFIG_ZAMDACH_WPDSID_PRESSURE, "pressure", press);
+        }
+
+        if (raing > -0.1) {
+          ESP_LOGI(TAG, "Rain: %.3f mm", raing);
+          submit_to_wpd(CONFIG_ZAMDACH_WPDSID_RAINGAUGE1, "precipitation", raing);
+        }
+
+        network_off();
+        long howmuchtosleep = (lastmeasts + 60) - time(NULL);
+        ESP_LOGI(TAG, "will now enter sleep mode for %ld seconds", howmuchtosleep);
+        if (howmuchtosleep > 0) {
+          /* This is given in microseconds */
+          esp_sleep_enable_timer_wakeup(howmuchtosleep * (int64_t)1000000);
+          esp_light_sleep_start();
+        }
+      } else { // Delay for a short time
+        vTaskDelay(pdMS_TO_TICKS(1000));
+      }
     }
 
 }
-
