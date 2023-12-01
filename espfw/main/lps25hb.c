c959c35f79018435c6a596d3cde5dc98d5c6b591
Michael 'PoempelFox' Meier <git@mulder.franken.de>
2023-12-01 20:39:49 +0100

    try to detect when lps25hb has spontanously reset, and reinitialize it in that case.

--- espfw/main/lps25hb.c
+++ espfw/main/lps25hb.c
@@ -41,12 +41,24 @@ void lps25hb_init(i2c_port_t port)
 double lps25hb_readpressure(void)
 {
     uint8_t prr[3];
-    int lpshadreaderr = 0;
+    static int lpsnonsensereadcounter = 0;
     if (lps25hb_register_read(0x80 | 0x28, &prr[0], 3) != ESP_OK) {
       /* There was an I2C read error - return a negative pressure to signal that. */
       return -999999.9;
     }
-    ESP_LOGI("lps25hb.c", "LPS25HB read - err %d values %02x%02x%02x", lpshadreaderr, prr[0], prr[1], prr[2]);
+    if ((prr[2] == 0x2f) && (prr[1] == 0x80) && (prr[0] == 0x00)) {
+      /* This is the poweron-default-value of the register, meaning the
+       * thing might have reset. */
+      lpsnonsensereadcounter++;
+      if (lpsnonsensereadcounter > 10) { /* OK, we read nonsense 10 times in a row. */
+        /* Lets try to resend the config command - we just call our own init func for that. */
+        lps25hb_init(lps25hbi2cport);
+        lpsnonsensereadcounter = 0;
+      }
+      /* In any case, 760hPa is a nonsense-value - return negative pressure to signal that. */
+      return -999999.9;
+    }
+    ESP_LOGI("lps25hb.c", "LPS25HB read - values %02x%02x%02x", prr[0], prr[1], prr[2]);
     double press = (((uint32_t)prr[2]  << 16)
                   + ((uint32_t)prr[1]  <<  8)
                   + ((uint32_t)prr[0]  <<  0)) / 4096.0;
